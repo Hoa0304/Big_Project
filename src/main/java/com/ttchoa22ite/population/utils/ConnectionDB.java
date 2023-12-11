27ccaca10b7efeb63d720bbef5e53eeeb4f92915
Hoa0304 <tranthicamhoa2k4@gmail.com>
2023-12-11 21:27:11 +0700

    fix bug

--- src/main/java/com/ttchoa22ite/population/utils/ConnectionDB.java
+++ src/main/java/com/ttchoa22ite/population/utils/ConnectionDB.java
@@ -12,7 +12,7 @@ public class ConnectionDB {
     public static Connection DAO() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/demographic_management", "root", "");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bigproject", "root", "");
             return con;
         } catch (ClassNotFoundException | SQLException ex) {
             System.err.println("Connect : "+ex.getMessage());
